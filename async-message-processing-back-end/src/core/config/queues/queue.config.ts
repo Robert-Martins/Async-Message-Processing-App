cef1e1f03a658647cce03f27597782c118e19d80
Robert-Martins <robertcantares@gmail.com>
2025-07-28 20:45:52 -0300

    feat: Define queue configuration function using config service

---
+++ async-message-processing-back-end/src/core/config/queues/queue.config.ts
@@ -0,0 +1,22 @@
+import { ConfigService } from "@nestjs/config";
+import { ClientProvider, Transport } from "@nestjs/microservices";
+
+export const provideQueueConfiguration = async (
+    queueName: string,
+  configService: ConfigService,
+): Promise<ClientProvider> => ({
+    transport: Transport.RMQ,
+    options: {
+        urls: [`${configService.get<string>('queue.protocol')}://${configService.get<string>('queue.username')}:${configService.get<string>('queue.password')}@${configService.get<string>('queue.url')}`],
+        queue: `${configService.get<string>('queue.prefix')}.${queueName}.${configService.get<string>('queue.owner')}`,
+        queueOptions: { 
+            durable: true,
+            exclusive: false,
+            autoDelete: false
+        },
+        socketOptions: {
+            heartbeatIntervalInSeconds: 60,
+            reconnectTimeInSeconds: 5
+        }
+    },
+});
