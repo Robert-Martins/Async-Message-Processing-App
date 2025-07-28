b5dbbb8f82e5147120400d6395441f3d3a0913af
Robert-Martins <robertcantares@gmail.com>
2025-07-28 20:33:03 -0300

    feat: Configure NestJS configure module to use env vars

--- async-message-processing-back-end/src/app.module.ts
+++ async-message-processing-back-end/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { NotificacaoModule } from './modules/notificacao.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './core/config/environment/configuration';
 
 @Module({
   imports: [NotificacaoModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
   controllers: [AppController],
 })
 export class AppModule {}
