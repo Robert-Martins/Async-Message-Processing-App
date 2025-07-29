2aaf396661521daa56b01d76f134787d07e8e3ea
Robert-Martins <robertcantares@gmail.com>
2025-07-28 23:20:33 -0300

    feat: Organize Atomic Design components

--- async-message-processing-front-end/src/app/shared/components/atoms/atoms.module.ts
+++ async-message-processing-front-end/src/app/shared/components/atoms/atoms.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from './forms/forms.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+  ],
+  exports: [
+    FormsModule,
   ]
 })
 export class AtomsModule { }
--- async-message-processing-front-end/src/app/shared/components/molecules/molecules.module.ts
+++ async-message-processing-front-end/src/app/shared/components/molecules/molecules.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListsModule } from './lists/lists.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ListsModule
+  ],
+  exports: [
+    ListsModule
   ]
 })
 export class MoleculesModule { }
