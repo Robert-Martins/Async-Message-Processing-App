b7753b6e3405cb768be22669efcf515efd29ba99
Robert-Martins <robertcantares@gmail.com>
2025-07-28 22:02:38 -0300

    feat: Define notification status and model

---
+++ async-message-processing-front-end/src/app/core/enums/status-notificacao.enum.ts
@@ -0,0 +1,7 @@
+export enum StatusNotificacao {
+
+    AGUARDANDO_PROCESSAMENTO = "Aguardando Processamento",
+    PROCESSADO_SUCESSO = "Processado com Sucesso",
+    FALHA_PROCESSAMENTO = "Falha no Processamento",
+
+}
